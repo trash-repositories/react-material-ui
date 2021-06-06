cd8d0918595c2017fb699c69683f1625f81787dd
jo-narare <nare6004@gmail.com>
2021-06-07 00:03:40 +0900

    라우터 설정 및 템플릿 적용

---
+++ src/listItems.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ViewComfyIcon from '@material-ui/icons/ViewComfy';
+
+import { Link } from 'react-router-dom';
+
+const aTagStyle = { textDecoration: 'none', color: 'inherit' };
+
+export const LayoutListItems = (
+  <div>
+    <ListSubheader inset>Layout</ListSubheader>
+    <Link to="/" style={aTagStyle}>
+      <ListItem button>
+        <ListItemText inset primary="Box" />
+      </ListItem>
+    </Link>
+    <Link to="/about" style={aTagStyle}>
+      <ListItem button>
+        <ListItemText inset primary="Container" />
+      </ListItem>
+    </Link>
+    <ListItem button>
+      <ListItemIcon>
+        <ViewComfyIcon />
+      </ListItemIcon>
+      <ListItemText primary="Grid" />
+    </ListItem>
+  </div>
+);
+
+export const InputListItems = (
+  <div>
+    <ListSubheader inset>Inputs</ListSubheader>
+    <ListItem button>
+      <ListItemText inset primary="Button" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Button Group" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Checkbox" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Floating action button" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Date / Time pickers" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Radio" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Select" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Slider" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Switch" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Text Field" />
+    </ListItem>
+    <ListItem button>
+      <ListItemText inset primary="Transfer List" />
+    </ListItem>
+  </div>
+);
