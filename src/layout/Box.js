dd6d29518fe438e1ebfa3d0ef02639911e6d4dc4
jo-narare <nare6004@gmail.com>
2021-06-07 16:49:01 +0900

    박스 컴포넌트 수정

--- src/layout/Box.js
+++ src/layout/Box.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { Divider, Box, Chip } from '@material-ui/core';
-
-const firstExample = `
-  <Box component="span" m={1}>
-  </Box>
-`;
+import { Divider, Box } from '@material-ui/core';
 
 const BoxComponent = () => {
   return (
@@ -13,11 +8,12 @@ const BoxComponent = () => {
       <p>Box 구성 요소는 대부분의 CSS 유틸리티 요구사항에 대한 wrapper 컴포넌트 역할을 합니다.</p>
       <Divider />
       <h1>사용 예</h1>
-      <Box component="span" m={1}></Box>
-      <Chip label={firstExample} href="#chip" clickable variant="outlined" />
+      <Box component="span" m={1}>
+        {`<Box component="span" m={1}></Box>`}
+      </Box>
       <Divider />
       <h1>API</h1>
-      <Chip label="import Box from '@material-ui/core/Box';" href="#chip" clickable variant="outlined" />
+      {`import Box from '@material-ui/core/Box'`}
     </div>
   );
 };
